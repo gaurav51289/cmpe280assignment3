eee3642b4d0187b7a4ddef52ca8b315b277cfda8
Ekta Sorathia <ektasorathia@Ektas-MacBook-Pro.local>
2017-10-09 20:21:00 -0700

    Images added for all the tabs

--- routes/items.js
+++ routes/items.js
@@ -7,43 +7,107 @@ router.get('/sofas', function(req, res, next) {
   var sofas = [{
       url:'./images/items/sofa1.png',
       name: 'Green Sofa',
-      desc: 'Awesome sofa in green color.',
+      desc: 'This sofa is comfortable regardless if you want to sit, lean back or lie down. The loose back cushions make it easy to adjust the seat depth and the back support.',
       price: '$899'
   },{
       url:'./images/items/sofa2.png',
       name: 'Red Sofa',
-      desc: 'Awesome sofa in red color.',
+      desc: 'The cover is easy to keep clean as it can be wiped clean with a damp cloth..',
       price: '$799'
   },{
       url:'./images/items/sofa3.png',
       name: 'Black Sofa',
-      desc: 'Awesome sofa in black color.',
+      desc: 'Seat cushions have a layer of memory foam that softly follows the contours of your body and gives comfortable support where needed..',
+      price: '$599'
+  },{
+      url:'./images/items/sofa4.png',
+      name: 'Antique Sofa',
+      desc: 'The outer surfaces are covered in a durable coated fabric with the same look and feel as leather.',
       price: '$599'
   }];
 
   res.send(sofas);
 });
 
-router.get('/tvmedia', function(req, res, next) {
+router.get('/chair', function(req, res, next) {
 
-    var tvmedia = [{
-        url:'./images/items/sofa1.png',
-        name: 'Green Sofa',
-        desc: 'Awesome sofa in green color.',
-        price: '$899'
+    var chair = [{
+        url:'./images/items/chair1.png',
+        name: 'Grey Cushion Armchair',
+        desc: 'Relaxing cushion armchair in light grey.',
+        price: '$119'
     },{
-        url:'./images/items/sofa2.png',
-        name: 'Red Sofa',
-        desc: 'Awesome sofa in red color.',
-        price: '$799'
+        url:'./images/items/chair2.png',
+        name: 'Royal Blue Armchair',
+        desc: 'Relaxing cushion armchair in royal blue color.',
+        price: '$129'
     },{
-        url:'./images/items/sofa3.png',
-        name: 'Black Sofa',
-        desc: 'Awesome sofa in black color.',
-        price: '$599'
+        url:'./images/items/chair3.png',
+        name: 'Maroon Armchair',
+        desc: 'Relaxing cushion armchair in maroon color.',
+        price: '$115'
+    },{
+        url:'./images/items/chair6.png',
+        name: 'Antique looking chair',
+        desc: 'Antique looking chair for living room.',
+        price: '$149'
     }];
 
-    res.send(tvmedia);
+    res.send(chair);
+});
+
+router.get('/sides', function(req, res, next) {
+
+    var sides = [{
+        url:'./images/items/table1.png',
+        name: 'Wooden Brown Table',
+        desc: 'Wooden Side Table.',
+        price: '$219'
+    },{
+        url:'./images/items/table2.png',
+        name: 'Beige Table',
+        desc: 'Wooden table in Beige in a stylish form.',
+        price: '$399'
+    },{
+        url:'./images/items/table3.png',
+        name: 'Woody Coffee Table',
+        desc: 'Wooden Table that contains more space.',
+        price: '$599'
+    },{
+        url:'./images/items/table4.png',
+        name: 'Side Table',
+        desc: 'Steel, Pigmented epoxy/polyester powder coating.',
+        price: '$299'
+    }];
+
+    res.send(sides);
+});
+
+router.get('/light', function(req, res, next) {
+
+    var light = [{
+        url:'./images/items/lamp1.png',
+        name: 'Hektar Floor lamp',
+        desc: 'The oversized lamp head gives both a good concentrated light for reading and good general light for smaller areas.',
+        price: '$49.99'
+    },{
+        url:'./images/items/lamp2.png',
+        name: 'Stylish Floor Lamp',
+        desc: 'Gives a soft glowing light, that gives your home a warm and welcoming atmosphere.',
+        price: '$39.9'
+    },{
+        url:'./images/items/lamp8.png',
+        name: 'Angland Floor Lamp',
+        desc: 'You can create a soft, cozy atmosphere in your home with a textile shade that spreads a diffused and decorative light.',
+        price: '$59.9'
+    },{
+        url:'./images/items/lamp7.png',
+        name: 'Alang Floor Lamp',
+        desc: 'The height is adjustable to suit your lighting needs.',
+        price: '$34.99'
+    }];
+
+    res.send(light);
 });
 
 module.exports = router;
