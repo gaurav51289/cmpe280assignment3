3c5bca3a689757aa6554752a90fb7659530da3c7
Ekta Sorathia <ektasorathia@Ektas-MacBook-Pro.local>
2017-10-09 19:14:44 -0700

    widget-1

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    $( "#tabs" ).tabs();
     $.ajax({
         url:'http://localhost:3000/items/sofas',
         success: function (result) {
@@ -6,29 +8,19 @@ $(document).ready(function () {
         }
     });
 
-    $('.nav li a').click(function(e) {
-        $('.nav li a.active').removeClass('active');
-        $(this).addClass('active');
-        e.preventDefault();
-
+    $('#tabs ul li a').click(function(e) {
         switch (e.target.id){
-            case 'nav1':
-                $.ajax({
-                   url:'http://localhost:3000/items/sofas',
-                   success: function (result) {
-                       listItems(result);
-                   }
-                });
-
+            case '1':
+                listItems('tabs-1', 'sofas');
                 break;
-            case 'nav2':
-                listItems(2);
+            case '2':
+                listItems('tabs-2', 'sides');
                 break;
-            case 'nav3':
-                listItems(3);
+            case '3':
+                listItems('tabs-3', 'tvmedia');
                 break;
-            case 'nav4':
-                listItems(4);
+            case '4':
+                listItems('tabs-4', 'light');
                 break;
             default:
                 break;
@@ -36,7 +28,7 @@ $(document).ready(function () {
     });
 });
 
-function init()
+function createDraggablesItems()
 {
     var draggableItem = $(".draggable");
     draggableItem.draggable({
@@ -48,27 +40,31 @@ function init()
     resizableItem.resizable();
 }
 
-function listItems(items) {
-    var itemList = $(".itemList");
-    itemList.empty();
-
-    items.forEach(function (item) {
-        itemList.append('<div class="row no-margin">\n' +
-            '                            <div class="media">\n' +
-            '                                <div class="draggable">\n' +
-            '                                    <div class="resizable">\n' +
-            '                                        <img class="d-flex mr-3" src="'+item.url+'" alt="Sofa" style="width: 100%">\n' +
-            '                                    </div>\n' +
-            '                                </div>\n' +
-            '                                <div class="media-body">\n' +
-            '                                    <h5 class="mt-0">'+item.name+'</h5>\n' +
-            '                                    <p>'+item.price+'</p>\n' +
-            '                                    <p>This is an awesome Green Sofa.</p>\n' +
-            '                                </div>\n' +
-            '                            </div>\n' +
-            '                        </div>');
-    });
-
-    init();
+function listItems(id, endpoint) {
+    var itemList = $("#"+id);
+    if(itemList.children().length == 0){
+        $.ajax({
+            url:'http://localhost:3000/items/'+endpoint,
+            success: function (results) {
+                results.forEach(function (item) {
+                    itemList.append('<div class="row no-margin">\n' +
+                        '                            <div class="media">\n' +
+                        '                                <div class="draggable">\n' +
+                        '                                    <div class="resizable">\n' +
+                        '                                        <img class="d-flex mr-3" src="'+item.url+'" alt="Sofa" style="width: 100%">\n' +
+                        '                                    </div>\n' +
+                        '                                </div>\n' +
+                        '                                <div class="media-body">\n' +
+                        '                                    <h5 class="mt-0">'+item.name+'</h5>\n' +
+                        '                                    <p>'+item.price+'</p>\n' +
+                        '                                    <p>This is an awesome Green Sofa.</p>\n' +
+                        '                                </div>\n' +
+                        '                            </div>\n' +
+                        '                        </div>');
+                });
+            }
+        });
+    }
+    createDraggablesItems();
 }
 
--- routes/items.js
+++ routes/items.js
@@ -24,4 +24,26 @@ router.get('/sofas', function(req, res, next) {
   res.send(sofas);
 });
 
+router.get('/tvmedia', function(req, res, next) {
+
+    var tvmedia = [{
+        url:'./images/items/sofa1.png',
+        name: 'Green Sofa',
+        desc: 'Awesome sofa in green color.',
+        price: '$899'
+    },{
+        url:'./images/items/sofa2.png',
+        name: 'Red Sofa',
+        desc: 'Awesome sofa in red color.',
+        price: '$799'
+    },{
+        url:'./images/items/sofa3.png',
+        name: 'Black Sofa',
+        desc: 'Awesome sofa in black color.',
+        price: '$599'
+    }];
+
+    res.send(tvmedia);
+});
+
 module.exports = router;
